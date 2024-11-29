0ccf54efb64c2c15c3e15bc72f0c7f3ac9b86439
Anamika <anamikap6840@gmail.com>
2024-11-29 12:19:45 +0530

    Deleted some old unwanted files

--- backend/src/interfaces/routes.ts
+++
@@ -1,8 +0,0 @@
-import express  from "express";
-import { signUpController } from "./UserController";
-
-const router = express.Router();
-
-router.post('/signup', signUpController);
-
-export default router;
